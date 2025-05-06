6b53e722264033d949219dcac732c7a02a33e6c2
Turdali <turdalihasanboyev70@gmail.com>
2025-05-06 16:18:35 +0500

    `Added Contact model with fields for name, email, phone number, subject, message, and file upload.`

--- apps/contact/models.py
+++ apps/contact/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from ckeditor.fields import RichTextField
+
+from apps.common.models import BaseModel
+
+
+class Contact(BaseModel):
+    name = models.CharField(max_length=225, db_index=True)
+    email = models.EmailField(max_length=225, db_index=True, unique=True)
+    phone_number = models.CharField(max_length=225, db_index=True, unique=True)
+    subject = models.CharField(max_length=225, db_index=True, unique=True)
+    message = RichTextField(null=True, blank=True)
+    file = models.FileField(upload_to='contact_files/', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Aloqa'
+        verbose_name_plural = 'Aloqalar'
+
+    def __str__(self):
+        return f'{self.name} - {self.subject}'
